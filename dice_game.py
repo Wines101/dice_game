bb0c743f49ecbc420ccb1b7e38965d61637fab34
BuildTools <unconfigured@null.spigotmc.org>
2024-09-30 22:56:17 -0500

    Added pygame mainloop

--- dice_game.py
+++ dice_game.py
@@ -1,3 +1,32 @@
+from src import dice_class
+import pygame
 
 def main():
-    
+    dice_game = dice_class.Dice(2)
+
+    pygame.init() 
+
+    screen = pygame.display.set_mode([500,500])
+    pygame.display.set_mode((500, 500))
+    pygame.display.set_caption('Dice Game')
+
+    running = True
+    while running: 
+ 
+        for event in pygame.event.get(): 
+        
+            if event.type == pygame.QUIT: 
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if 
+                    print("Left mouse")
+        screen.fill((255,255,255))
+        pygame.draw.circle(screen, (0, 0, 255), (250,250), 75)
+
+        pygame.display.flip()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
--- src/dice_class.py
+++ src/dice_class.py
@@ -1,9 +1,7 @@
 import pygame
 class Dice:
     def __init__(self, number_of_dice):
-        self._number_of_dice = number_of_dice
-        self._big_font = pygame.font.SysFont("comicsansms", 36)
-        self._small_font = pygame.font.SysFont("comicsansms", 25)
+        self._number_of_dice = int(number_of_dice)
     
     def roll_dice(self):
-        
+        pass
